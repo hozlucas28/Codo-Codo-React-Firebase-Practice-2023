03cefd254c98534e515d0793d161ab60c2f7b4fa
hozlucas28 <hozlucas28@gmail.com>
2023-06-04 03:05:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Router } from './routes/app.routes'
 
+import GlobalProvider from './contexts/GlobalProvider.jsx'
+
 import AuthProvider from './auth/context/AuthProvider'
 
 const App = () => {
 	return (
-		<AuthProvider>
-			<RouterProvider router={Router} />
-		</AuthProvider>
+		<GlobalProvider>
+			<AuthProvider>
+				<RouterProvider router={Router} />
+			</AuthProvider>
+		</GlobalProvider>
 	)
 }
 
