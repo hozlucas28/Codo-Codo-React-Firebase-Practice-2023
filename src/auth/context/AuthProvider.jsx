e6f4f36615dbf751086e0e46a8d24c25fc02e43c
hozlucas28 <hozlucas28@gmail.com>
2023-06-05 02:02:18 -0300

    Remove unnecessary import

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import { useReducer } from 'react'
 
 import { AUTH_KEY_LOCAL_STORAGE, AUTH_TYPES, authInitialState, authInitializer, authReducer } from '../reducer/authReducer'
 import { AuthContext } from './AuthContext'
