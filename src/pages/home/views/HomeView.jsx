30e04a480512498cc2501339079cbc1943715c33
hozlucas28 <hozlucas28@gmail.com>
2023-06-05 17:11:29 -0300

    Add illustrations

--- src/pages/home/views/HomeView.jsx
+++ src/pages/home/views/HomeView.jsx
@@ -1,11 +1,16 @@
 import Profile from '../components/Profile.jsx'
 
-import { container } from './HomeView.module.css'
+import { ReactComponent as LineWithoutSense01 } from '../../../static/illustrations/line-without-sense-1.svg'
+import { ReactComponent as LineWithoutSense02 } from '../../../static/illustrations/line-without-sense-2.svg'
+
+import { container, illustration01, illustration02 } from './HomeView.module.css'
 
 const HomeView = () => {
 	return (
 		<div className={container}>
 			<Profile />
+			<LineWithoutSense01 className={illustration01} />
+			<LineWithoutSense02 className={illustration02} />
 		</div>
 	)
 }
--- src/pages/login/views/LoginView.jsx
+++ src/pages/login/views/LoginView.jsx
@@ -1,11 +1,16 @@
 import LoginForm from '../components/LoginForm'
 
-import { view } from './LoginView.module.css'
+import { ReactComponent as LineWithoutSense01 } from '../../../static/illustrations/line-without-sense-1.svg'
+import { ReactComponent as LineWithoutSense02 } from '../../../static/illustrations/line-without-sense-2.svg'
+
+import { illustration01, illustration02, view } from './LoginView.module.css'
 
 const LoginView = () => {
 	return (
 		<div className={view}>
 			<LoginForm />
+			<LineWithoutSense01 className={illustration01} />
+			<LineWithoutSense02 className={illustration02} />
 		</div>
 	)
 }
