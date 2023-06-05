3e8a86ac1806cb3b7fd00dca1bda7f8f46a70e5c
hozlucas28 <hozlucas28@gmail.com>
2023-06-05 02:01:59 -0300

    Remove global context

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Router } from './routes/app.routes'
 
-import GlobalProvider from './contexts/GlobalProvider.jsx'
-
 import AuthProvider from './auth/context/AuthProvider'
 
 const App = () => {
 	return (
-		<GlobalProvider>
-			<AuthProvider>
-				<RouterProvider router={Router} />
-			</AuthProvider>
-		</GlobalProvider>
+		<AuthProvider>
+			<RouterProvider router={Router} />
+		</AuthProvider>
 	)
 }
 
