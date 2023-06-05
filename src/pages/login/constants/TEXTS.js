b84b78dad5d7db4dcd874ce27d74c0b2828ce213
hozlucas28 <hozlucas28@gmail.com>
2023-06-05 02:04:39 -0300

    Update Login page

---
+++ src/pages/login/constants/TEXTS.js
@@ -0,0 +1,33 @@
+export const TEXTS = {
+	form: {
+		header: {
+			title: 'User login',
+			subTitle: 'Hey, Enter your details to get sign in to your account',
+		},
+
+		fields: {
+			email: {
+				placeholder: 'Email',
+			},
+
+			password: {
+				placeholder: 'Password',
+			},
+		},
+
+		buttons: {
+			signIn: {
+				text: 'Sign in',
+			},
+
+			divider: {
+				text: 'Or sign in with',
+			},
+
+			signInWithGoogle: {
+				text: 'Google',
+				error: 'An Error occurred on sign in with Google',
+			},
+		},
+	},
+}
